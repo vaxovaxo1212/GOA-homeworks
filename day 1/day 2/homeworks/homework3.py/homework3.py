4290dace9201270f666c66d4f152e9df4d4d8bb2
vaxovaxo1212 <vaxtangitukvadze@gmail.com>
2025-10-17 22:35:21 +0400

    second commit

---
+++ day 1/day 2/homeworks/homework3.py/homework3.py
@@ -0,0 +1,25 @@
+W = 3 + 4j
+print (type(W))
+
+# string data type
+name = "vaxo"
+print(type(name))
+
+# List # of data Types (Ordered and Mutable)
+fruits = ["apple", "watermelon", "orange"]
+fruits.append("mango")
+fruits [1] = "banana"
+print (fruits)
+print(type(fruits))
+
+# tuple data types(ordered and immutable)
+coordinates = (10, 20)
+colors = ("red," "blue," "yellow")
+
+print(type(coordinates))
+print(colors)
+
+# set data types (unordered and mutable)
+numbers = {1, 2, 3, 4, 5}
+print(type(numbers))
+print(type(numbers))
